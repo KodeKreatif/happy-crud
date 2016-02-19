12be1d85836da7ecd0934dd2558265cb3de6f6b3
Herpiko Dwi Aguno <herpiko@gmail.com>
2016-02-19 16:30:34 +0700

    Add mongoDb abstarction model

--- tests/index.js
+++ tests/index.js
@@ -4,3 +4,8 @@ const server = require("./server");
 const TestA = require('./testA');
 const testA = new TestA(server);
 testA.test();
+
+const TestMongo = require('./testMongo');
+new TestMongo(server).then((testMongo) => {
+  testMongo.test();
+});
